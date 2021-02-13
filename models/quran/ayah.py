46defdba19c5953d2d70d2b8be7249a5e7028293
axeemhaider <axeemhaider@gmail.com>
2021-02-13 21:57:03 +0500

    adding uci

--- models/quran/ayah.py
+++ models/quran/ayah.py
@@ -1,11 +1,12 @@
 from fireo.models import Model
-from fireo.fields import IDField, NumberField, MapField
+from fireo.fields import IDField, NumberField, MapField, TextField
 
 class Ayah(Model):
     id = IDField()
     number = NumberField()
     surah_number = NumberField()
     content = MapField()
+    uci = TextField(required=True)
 
     class Meta:
         collection_name = "quran"
--- quran_importer.py
+++ quran_importer.py
@@ -1,5 +1,6 @@
 from os import listdir
 from os.path import isfile, join
+from uci import UCI
 
 import fireo
 from models.quran.ayah import Ayah
@@ -11,9 +12,10 @@ ur_quran = open('./data/quran/ur.maududi_rtl.txt', "r", encoding="utf8")
 ayah_batch = fireo.batch()
 count = 0
 line_count = 0
+uci = UCI(246655)
 
 for ar, en, ur in zip(ar_quran, en_quran, ur_quran):
-    
+
     if not en.strip():
         print('English file end')
         break
@@ -22,26 +24,19 @@ for ar, en, ur in zip(ar_quran, en_quran, ur_quran):
     _, _, ur_text = ur.strip().split('|')
     _, _, ar_text = ar.strip().split('|')
 
-    print(' writing line...' + str(line_count) + ' ...Surah ' + surah_number + ' ...Ayah ' + ayah_number)
+    print(' writing line...' + str(line_count) + ' ...Surah ' +
+          surah_number + ' ...Ayah ' + ayah_number)
     line_count += 1
 
     ayah = Ayah()
     ayah.id = str(surah_number) + '-' + str(ayah_number)
     ayah.number = int(ayah_number)
     ayah.surah_number = int(surah_number)
+    ayah.uci = uci.next
     ayah.content = {
-        "arabic": {
-            "text": ar_text,
-            "direction": "rtl"
-        },
-        "english": {
-            "text": en_text,
-            "direction": "ltr"
-        },
-        "urdu": {
-            "text": ur_text,
-            "direction": "rtl"
-        },
+        "arabic": ar_text,
+        "urdu": ur_text,
+        "english": en_text
     }
     ayah.save(batch=ayah_batch)
 
@@ -52,4 +47,5 @@ for ar, en, ur in zip(ar_quran, en_quran, ur_quran):
         count = 0
 
 print('============Complete=============================')
+print(uci.end_on)
 ayah_batch.commit()
