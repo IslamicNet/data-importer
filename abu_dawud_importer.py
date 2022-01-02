5c96fcf39c2d7fd26b222fe9d21dcf47907d45d5
AxeemHaider <axeemhaider@gmail.com>
2022-01-01 21:35:44 -0800

    abu dawd complete

--- abu_dawud_importer.py
+++ abu_dawud_importer.py
@@ -3,40 +3,48 @@
 # end number: 367891
 # Book No: 4
 #########################
-import fireo
+import pymongo
 import json
 from uci import UCI
-from models.hadiths.abudawud import AbuDawud
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["islamic_content_v1"]
+mycol = mydb["abu_dawud"]
 
 abudawud_file = open('./data/hadiths/abu_dawud.json', "r", encoding="utf8")
 abudawud = json.load(abudawud_file)
 
-hadith_batch = fireo.batch()
+hadith_batch = []
 count = 0
 overall_count = 0
 uci = UCI(362617)
 
 for hadith in abudawud:
-    abudawud = AbuDawud()
-    abudawud.id = str(hadith['hadees_number'])
-    abudawud.hadith_number = int(hadith['hadees_number'])
-    abudawud.book_number = int(hadith['Kitab_ID'])
-    abudawud.book_name = {
-        "urdu": hadith['Kitab'],
-        "english": hadith['Kitab_Eng']
-    }
-    abudawud.chapter = {
-        "urdu": hadith['Baab'],
-        "english": hadith['Baab_Eng']
-    }
-    abudawud.text = {
+
+    docUCI = uci.next
+
+    abudawud = {
+        "_id": docUCI,
+        "hadithNumber": int(hadith['hadees_number']),
+        "bookNumber": int(hadith['Kitab_ID']),
+        "bookName": {
+            "urdu": hadith['Kitab'],
+            "english": hadith['Kitab_Eng']
+        },
+        "chapter": {
+            "urdu": hadith['Baab'],
+            "english": hadith['Baab_Eng']
+        },
         "arabic": hadith['Arabic'],
-        "urdu": hadith['Ravi'] + hadith['Urdu'],
-        "english": hadith['English']
+        "trnaslations":  {
+            "urdu": hadith['Ravi'] + hadith['Urdu'],
+            "english": hadith['English']
+        },
+        "isSahih":  bool(hadith['Sahih_Zaeef']),
+        "uci": docUCI
     }
-    abudawud.is_sahih = bool(hadith['Sahih_Zaeef'])
-    abudawud.uci = uci.next
-    abudawud.save(batch=hadith_batch)
+
+    hadith_batch.append(abudawud)
 
     count += 1
     overall_count += 1
@@ -44,11 +52,12 @@ for hadith in abudawud:
     print("Over all complete " + str(overall_count))
 
     if count > 400:
-        hadith_batch.commit()
+        mycol.insert_many(hadith_batch)
+        hadith_batch = []
         count = 0
 
 print("========== COMPLETE ===========")
 print("Overall count ", overall_count)
 print("UCI end on ", uci.end_on)
 
-hadith_batch.commit()
+mycol.insert_many(hadith_batch)
